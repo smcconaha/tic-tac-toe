6cbb5819820504db9d12279a4794222779ab38e5
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-10-05 20:09:52 +0000

    Added basic function in sandbox to loop through array and provide the index of X or O

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,1 +1,20 @@
-//keep
+//create an array
+let boardState = ['','','','','','','',''];
+//given a array with Xs and Os, return the index numbers where those values occur
+const testArray = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O'];
+
+function testArray (array) {
+    let arrayX = [];
+    let arrayO = [];
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] === 'X') {
+            arrayX.push(i);
+        } else if
+            (array[i] === 'O') {
+                arrayO.push(i);
+            }
+        };
+    return arrayX;
+};
+
+
