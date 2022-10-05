2c718f137563f71bcf7d516f6c5f30de3acb99a2
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-10-05 20:46:02 +0000

    Added objects definitions to sandbox file

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,5 +1,3 @@
-//create an array
-let boardState = ['','','','','','','',''];
 //given a array with Xs and Os, return the index numbers where those values occur
 const testArray = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O'];
 
@@ -17,4 +15,28 @@ function testArray (array) {
     return arrayX;
 };
 
+function checkWinCond (array, val) {
+    //if (array.includes(winCriteria[0])) {
+    //    return "This is a winner";
+    //}
+    return array.some((arrVal) => val === arrVal);
+}
 
+//Object
+
+let gameState = {
+    playerTurn: 'x',
+    winCriteria: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 7, 8]                        
+    ],
+    boardState: ['','','','','','','',''],
+    gameActive: true,
+    currentState: 0,
+}
